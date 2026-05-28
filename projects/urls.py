de9ed7520b6121a4d3d1e1b36e75e0014b4c6f12
Davich02 <david.rabotarus@gmail.com>
2026-05-28 13:33:39 +0200

    Create get_all_projects view

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.urls import path
 
+from projects.views.projects import get_all_projects
+
 urlpatterns = [
-    path('', lambda request: HttpResponse('hello'))
+    path('', lambda request: HttpResponse('hello')),
+    path('projects/',get_all_projects),
 ]
+
